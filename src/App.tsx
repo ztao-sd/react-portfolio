825af05235523a8c4452e44b2a296ea1d19c59b9
Tao Zhou <ztao@DESKTOP-ORVGJ5L>
2024-06-30 21:09:10 -0400

    initial wireframe

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { AlertDialogDemo } from "./demo";
+
+function App() {
+  return (
+    <>
+      <div>
+        <AlertDialogDemo></AlertDialogDemo>
+      </div>
+    </>
+  );
+}
+
+export default App;
